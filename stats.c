07050b05382554673dd2688f7094c4b9d1363ee4
akash-cs13 <akashchandrashekar13@gmail.com>
2023-03-11 06:57:43 -0700

    Adding code outline and documentation for c1m1 assessment

--- stats.c
+++ stats.c
@@ -9,13 +9,12 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief Function prototypes for providing statics on an array of numbers, 
+ * and general practice in the "C" language
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Akash CS
+ * @date 11-03-23
  *
  */
 
@@ -29,15 +28,112 @@
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  unsigned int test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
+// Create Struct
+  stat Array;
+
+  // Initialize Struct
+  Array.numArray = test;
+  Array.arrayLength = SIZE;
+  Array.max = 0;
+  Array.min = 0xFF;
+  Array.mean = 0;
+  Array.median = 0;
+
+  // Process Data
+  sort_array(Array.numArray, Array.arrayLength);
+  Array.min = find_minimum(Array.numArray, Array.arrayLength);
+  Array.max = find_maximum(Array.numArray, Array.arrayLength);
+  Array.mean = find_mean(Array.numArray, Array.arrayLength);
+  Array.median = find_median(Array.numArray, Array.arrayLength);
+  print_array(Array.numArray, Array.arrayLength);
+  print_statistics(Array);
 }
 
-/* Add other Implementation File Code Here */
+void print_statistics(stat ArrayStats){
+  printf("Array Median: %i\n", ArrayStats.median);
+  printf("Array Mean: %i\n", ArrayStats.mean);
+  printf("Array Minimum: %i\n", ArrayStats.min);
+  printf("Array Maximum: %i\n", ArrayStats.max);
+}
+
+void print_array(int *arrayPtr, int arrayLength){
+  // Loop through and print the array
+  for(int i=0; i<arrayLength; i++){
+    printf("Array[%i]: %i\n", i, *(arrayPtr + sizeof(char)*i));
+  }
+  printf("\n");
+  
+}
+
+int find_median(int *arrayPtr, int arrayLength){
+  // Make sure array is sorted first
+  sort_array(arrayPtr, arrayLength);
+
+  // Case where array length is odd
+  if (arrayLength%2){
+    return arrayPtr[arrayLength/2];
+  }
+
+  // Case where array length is even.  Average between two middle values is necessary
+  int avg = (arrayPtr[arrayLength/2 - 1]+arrayPtr[arrayLength/2])/2;
+  return avg;
+}
+
+int find_mean(int *arrayPtr, int arrayLength){
+  int avg=0;
+
+  // Add up all the numbers in the array
+  for(int i=0; i<arrayLength; i++){
+    avg+= arrayPtr[i];
+  }
+  
+  // Integer division using Array Length
+  return (avg/arrayLength);
+}
+
+int find_minimum(int *arrayPtr, int arrayLength){
+  int minValue=0xFF;
+
+  for (int i=0; i<arrayLength; i++){
+    if (arrayPtr[i] < minValue){
+      minValue = arrayPtr[i];
+    }
+  }
+
+  return minValue;
+}
+
+int find_maximum(int *arrayPtr, int arrayLength){
+  int maxValue=0;
+
+  for (int i=0; i<arrayLength; i++){
+    if (arrayPtr[i] > maxValue){
+      maxValue = arrayPtr[i];
+    }
+  }
+
+  return maxValue;
+}
+
+void sort_array(int *arrayPtr, int arrayLength){
+  int i, j = 0;
+  int temp;
+  // Sort Array
+  for(i=0; i<arrayLength; i++){
+    for(j=i+1; j<arrayLength; j++){
+      if(arrayPtr[i]<arrayPtr[j]){
+        temp = arrayPtr[i];
+        arrayPtr[i]=arrayPtr[j];
+        arrayPtr[j]=temp;
+      }
+    }
+  }
+}
+
--- stats.h
+++ stats.h
@@ -9,32 +9,90 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Functions for providing statics on an array of numbers, and general 
+ * practice in the "C" language
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Akash CS
+ * @date 11-02-2023
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+
+typedef struct stats{
+    int *numArray;
+    int arrayLength;
+    int median;
+    int mean;
+    int max;
+    int min;
+} stat;
 
 /**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @brief Print out all the statistics related to the array
+ * 
+ * @param ArrayStats Struct containing array and associated statistics
+ * 
+ * @return Void()
  */
+void print_statistics(stat ArrayStats);
 
+/**
+ * @brief Prints the array
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ * 
+ * @return Void()
+ */
+void print_array(int *arrayPtr, int arrayLength);
+
+/**
+ * @brief Given an array pointer and length of array, returns the median value of the array
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ * @return int median value in array
+ */
+int find_median(int *arrayPtr, int arrayLength);
+
+/**
+ * @brief Given an array pointer and length of array, returns the average value of the array
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ * @return int average value in array
+ */
+int find_mean(int *arrayPtr, int arrayLength);
+
+/**
+ * @brief Given an array pointer and length of array, returns the minimum value in the array
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ * @return int minimum value in array
+ */
+int find_minimum(int *arrayPtr, int arrayLength);
+
+/**
+ * @brief Given an array pointer and length of array, returns the maximum value in the array
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ * @return int maximum value in array
+ */
+int find_maximum(int *arrayPtr, int arrayLength);
+
+/**
+ * @brief Sorts the array inplace from largest (index 0) to smallest (index n-1)
+ * 
+ * @param arrayPtr Pointer to the array of data
+ * @param arrayLength Number of elements in the array
+ */
+void sort_array(int *arrayPtr, int arrayLength);
 
 #endif /* __STATS_H__ */
